cdde4b9a244c17fee1c607648cb77b0cdc9d9ec5
annie bf <hianny.urt@bomfuturo.com.br>
2026-04-24 08:37:27 -0400

    testando tir e selenium

---
+++ mainTir.py
@@ -0,0 +1,45 @@
+import unittest
+from testecase import teste1
+import time
+import os
+import sys
+from time import sleep
+
+try:
+    # Abre o arquivo de log
+    log_file = open('Log\log.txt', 'w')
+    
+    # Redireciona a saída padrão e de erro
+    sys.stdout = sys.stderr = log_file
+
+    print('------------------------')
+    print('comecando o programa')
+    print('------------------------')
+
+    time.sleep(1)
+    suite = unittest.TestSuite()
+    print('entrando nos testes individuais')
+
+    suite.addTest(teste1('test_01'))
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(suite)  # Armazena o resultado para verificação
+    
+    print('------------------------')
+    print('finalizando o programa')
+    print('------------------------')
+
+except Exception as e:
+    print(f'ocorreu algum erro na execução do tir: {str(e)}')
+finally:
+    # Restaura os fluxos padrão primeiro
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
+    
+    # Fecha o arquivo de log para garantir que tudo foi escrito
+    log_file.close()
+    
+    # Aguarda um pouco para garantir que o sistema de arquivos tenha terminado as operações
+    sleep(5)
+    
+    # Agora chama a função salvar
